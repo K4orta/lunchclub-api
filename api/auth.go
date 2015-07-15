d444f6297b4e85eecf43291177e628f784dc2b60
Erik Wong <ewong@blizzard.com>
2015-07-14 20:35:17 -0700

    Added auth endpoints

--- api/auth.go
+++ api/auth.go
@@ -1,40 +1,70 @@
 package api
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 
+	"github.com/gorilla/sessions"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/facebook"
 )
 
 var fbClientID = os.Getenv("LCAPI_FB_APP_ID")
 var fbClientSecret = os.Getenv("LCAPI_FB_SECRET")
+var sessionSecret = os.Getenv("LCAPI_SESSION_SECRET")
+var store = sessions.NewCookieStore([]byte(sessionSecret))
 
-// AccessToken is a stuct that holds a token value and an extires time
+var conf = &oauth2.Config{
+	ClientID:     fbClientID,
+	ClientSecret: fbClientSecret,
+	Scopes:       []string{"public_profile", "email"},
+	Endpoint:     facebook.Endpoint,
+}
+
+// AccessToken is a stuct that holds a token value and an extires time.
 type AccessToken struct {
 	Token   string
 	Expires int64
 }
 
-// GetFBToken is a function that exchanges a code for an access token
+// GetFBToken is a function that exchanges a code for an access token.
 func GetFBToken(w http.ResponseWriter, req *http.Request) {
-
-}
-
-func RedirectFBLogin(w http.ResponseWriter, req *http.Request) {
-
-}
-
-// FBLoginURL returns a URL for the FB login promt
-func FBLoginURL(redirectURL string) string {
-	conf := &oauth2.Config{
-		ClientID:     fbClientID,
-		ClientSecret: fbClientSecret,
-		Scopes:       []string{"public_profile", "email"},
-		RedirectURL:  redirectURL,
-		Endpoint:     facebook.Endpoint,
+	q := req.URL.Query()
+	tok, err := conf.Exchange(oauth2.NoContext, q.Get("code"))
+	if err != nil {
+		fmt.Println(err)
 	}
+	resp, err := http.Get("https://graph.facebook.com/me?access_token=" + tok.AccessToken)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+	session, _ := store.Get(req, "lc-session")
+	session.Values["uname"] = "Adam"
+	session.Values["pw"] = "1234"
+	session.Save(req, w)
+
+	fmt.Fprint(w, string(respBody))
+}
+
+// RedirectFBLogin forwards the user to the Facebook login page.
+func RedirectFBLogin(w http.ResponseWriter, req *http.Request) {
+	http.Redirect(w, req, FBLoginURL("http://localhost:8001/auth"), http.StatusTemporaryRedirect)
+}
+
+// ConfirmSession forwards the user to the Facebook login page.
+func ConfirmSession(w http.ResponseWriter, req *http.Request) {
+	session, _ := store.Get(req, "lc-session")
+	fmt.Fprint(w, session.Values["uname"])
+}
+
+// FBLoginURL returns a URL for the FB login promt.
+func FBLoginURL(redirectURL string) string {
+	conf.RedirectURL = redirectURL
 	url := conf.AuthCodeURL("state", oauth2.AccessTypeOnline)
 	return url
 }
--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	// router.HandleFunc("/users", api.DeleteUser).Methods("DELETE")
 	router.HandleFunc("/login", api.RedirectFBLogin)
 	router.HandleFunc("/auth", api.GetFBToken)
+	router.HandleFunc("/confirm", api.ConfirmSession)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
