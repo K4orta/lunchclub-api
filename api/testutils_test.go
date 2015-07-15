b117c8582c00f30060e94f9c91e0cdf70318974f
Erik Wong <ewong@blizzard.com>
2015-07-14 20:35:57 -0700

    Add fake server test utility

---
+++ api/testutils_test.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+)
+
+func makeFakeServer(resp string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, resp)
+	}))
+}
